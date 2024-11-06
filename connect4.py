fda19adf7c6677ef12d3e7633501e9d5c9a7acf5
BKellems1 <132042111+BKellems1@users.noreply.github.com>
2024-11-06 15:39:18 -0500

    outline for thing
    description

---
+++ connect4.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+
+'''
+make a main menu
+-play button
+-color/character selection screen
+-button that leads to how to play
+-quit button
+
+gameplay menu
+-quit
+    -lead to main menu
+-restart
+    -clear board
+    -on win, spawn buttons to "play again" or "quit"
+-buttons ARE the columns
+-label up top that says who is playing
+
+
+    
+'''
+
